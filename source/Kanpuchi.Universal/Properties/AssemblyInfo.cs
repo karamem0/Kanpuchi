b3adf527a3f3c4d6c39aef14da54456a38ca4082
Takashi Shinohara <takashi.shinohara@outlook.com>
2016-01-29 23:30:23 +0900

    UWPアプリ用のプロジェクトの追加

---
+++ source/Kanpuchi.Universal/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("Kanpuchi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("karamem0")]
+[assembly: AssemblyProduct("Kanpuchi")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 karamem0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4FA2289F-BE6D-4EB5-BBE7-7B9FB1CE56EC")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
+
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
