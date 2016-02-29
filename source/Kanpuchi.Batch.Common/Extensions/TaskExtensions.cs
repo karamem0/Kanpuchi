bb28553d5b6b20f7f42467ebe4c3641754fcca35
Takashi Shinohara <takashi.shinohara@outlook.com>
2016-02-29 23:57:16 +0900

    TaskExtensionsクラスのリファクタリング

--- source/Kanpuchi.Batch.Common/Extensions/TaskExtensions.cs
+++ source/Kanpuchi.Batch.Common/Extensions/TaskExtensions.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 namespace Karamem0.Kanpuchi.Extensions {
 
     /// <summary>
-    /// <see cref="T:System.Threading.Tasks.Task`1"/> クラスの拡張メソッドを定義します。
+    /// <see cref="System.Threading.Tasks.Task{TResult}"/> クラスの拡張メソッドを定義します。
     /// </summary>
     public static class TaskExtensions {
 
         /// <summary>
-        /// 指定した <see cref="T:System.Threading.Tasks.Task`1"/> の実行を待機して実行結果を返します。
+        /// 指定した <see cref="System.Threading.Tasks.Task{TResult}"/> の実行を待機して実行結果を返します。
         /// </summary>
         /// <typeparam name="T">実行結果の型。</typeparam>
-        /// <param name="target">対象の <see cref="T:System.Threading.Tasks.Task`1"/>。</param>
+        /// <param name="target">対象の <see cref="System.Threading.Tasks.Task{TResult}"/>。</param>
         /// <returns>実行結果を示す値。</returns>
         public static T Wait<T>(this Task<T> target) {
             var result = default(T);
             target.ContinueWith(task => {
-                if (task.Exception == null) {
+                if (task.IsCanceled != true && task.IsFaulted != true) {
                     result = task.Result;
                 }
             }).Wait();
